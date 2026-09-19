8578fcd526f866e9be8ac337feadcc161aee84e6
Peep Ploom <ploom+RSID@rushstreetinteractive.com>
2026-09-19 19:05:29 +0300

    Build interactive Today OS prototype

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "terminal-001";
+const CACHE = "terminal-002";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
