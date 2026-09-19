f67f03fc0196842d7ef1c07dc4f3e94370d118fe
Peep Ploom <ploom+RSID@rushstreetinteractive.com>
2026-09-20 00:44:15 +0300

    Widen split keyboard layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "terminal-017";
+const CACHE = "terminal-018";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
