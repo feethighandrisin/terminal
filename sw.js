1e0633a06494a34fb1cbb59aebc2419c6ab1ed7d
Peep Ploom <ploom+RSID@rushstreetinteractive.com>
2026-09-19 20:54:31 +0300

    Soften completion glow field

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "terminal-006";
+const CACHE = "terminal-007";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
