8220269453e6d6c6987e1a95195f14c8667cd674
Peep Ploom <ploom+RSID@rushstreetinteractive.com>
2026-09-19 22:40:38 +0300

    Compact keyboard and reset cards per build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "terminal-008";
+const CACHE = "terminal-009";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
