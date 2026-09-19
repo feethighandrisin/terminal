b0eb7756683fd903b57c939cc24766e7613bb94c
Peep Ploom <ploom+RSID@rushstreetinteractive.com>
2026-09-19 20:01:09 +0300

    Increase completion glow visibility

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "terminal-004";
+const CACHE = "terminal-005";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
