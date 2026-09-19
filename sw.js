52e56cfd8656c3ce7dc8f8954ee0d400bead71f4
Peep Ploom <ploom+RSID@rushstreetinteractive.com>
2026-09-20 00:32:50 +0300

    Add gyroscopic spatial depth

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "terminal-015";
+const CACHE = "terminal-016";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
