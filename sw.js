6f0b904e78380f684bd164bf656ede18ce51db4d
Peep Ploom <ploom+RSID@rushstreetinteractive.com>
2026-09-19 23:39:38 +0300

    Extend keyboard afterglow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "terminal-011";
+const CACHE = "terminal-012";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
