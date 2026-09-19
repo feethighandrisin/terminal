6eacf5d3eabfea558e1e65785968e068d2eeb63d
Peep Ploom <ploom+RSID@rushstreetinteractive.com>
2026-09-20 00:18:13 +0300

    Refine keyboard control row

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "terminal-013";
+const CACHE = "terminal-014";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
